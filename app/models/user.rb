27d3ed49b41115e5cf49c1697fc3a975ddd76642
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-12-02 11:55:22 +0100

    added function and test for  add_star!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+	def add_star!
+		no_stars + 1
+	end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,10 @@ class UserTest < ActiveSupport::TestCase
     u = User.new
     assert u.no_stars = 0
   end
+
+  test "add_star! add one star" do
+    u = User.new
+    u.add_star!
+    assert u.no_stars = 1
+  end
 end
