8cfe54f909111bb772dd23107b1f431d3b4dfe42
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-12-02 11:46:23 +0100

    bundle, test for no_stars default

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,9 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "no_stars set to zero" do
+    u = User.new
+    assert u.no_stars = 0
+  end
 end
